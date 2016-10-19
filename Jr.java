35e0a3fd8e16a59a2542e58325254756778e07b1
chjp_615@qq.com <chjp615927>
2016-10-19 14:18:16 +0800

    add hash get

--- Jr.java
+++ Jr.java
@@ -1,13 +1,33 @@
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Map;
+
 public class Jr
 {
     public static void main(String args[])
     {
-        Jedis redis = new Jedis("127.0.0.1", 6379);
+        Jedis redis = new Jedis("192.168.10.191", 6379);
         // redis.auth(""); // password
         
-        redis.set("java-redis", "chen jian ping");
+        // redis.set("java-redis", "chen jian ping");
+        
+        Map<String, String> m = redis.hgetAll("National_brand");
+        int j = 0;
+        String str = "";
+        int v = 0;
+        for(Map.Entry entry : m.entrySet()) {
+            str = (String)entry.getKey().toString();
+            v = Integer.parseInt(entry.getValue().toString());
+            if(str.startsWith("laocaitangguo")) {
+                if(v > 1476406800 && v < 1476493200) {
+                    j = j + 1;
+                } else {
+                    System.out.println(str + "=" + v);
+                }
+ 
+            }
+        }
+        System.out.print(j);
     }
 }
