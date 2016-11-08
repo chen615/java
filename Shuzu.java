62722af85fff1e7bf8d628303c053a30031f51bc
chjp_615@qq.com <chjp615927>
2016-11-08 14:28:39 +0800

    修改

--- Shuzu.java
+++ Shuzu.java
@@ -8,13 +8,13 @@ public class Shuzu
         /*
         String[] str_array = new String[5];
         String array_string = Arrays.toString(str_array);
-        
+
         System.out.println(array_string);
-        
+
         String[] str = {"a", "b", "c", "d", "e"};
         System.out.println(Arrays.toString(str));
         */
-        
+
         String str = "102";
         boolean res = str.equals("102");
         if(res) {
